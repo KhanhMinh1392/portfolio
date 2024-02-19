530eadd5c2d1d92144e5a8015b9c61ba39041cf2
KhanhMinh1392 <81310329+KhanhMinh1392@users.noreply.github.com>
2024-02-20 06:46:17 +0700

    fix: bug animated number

--- src/components/shared/achievements-session.tsx
+++ src/components/shared/achievements-session.tsx
@@ -5,28 +5,23 @@ const AnimatedNumbers = dynamic(() => import("react-animated-numbers"), {
   ssr: false,
 });
 
-const achievementsList = [
-  {
-    metric: "Projects",
-    value: "6",
-    postfix: "+",
-  },
-  {
-    prefix: "~",
-    metric: "Users",
-    value: "100,000",
-  },
-  {
-    metric: "Awards",
-    value: "2",
-  },
-  {
-    metric: "Years",
-    value: "2",
-  },
-];
-
 export default function AchievementsSession() {
+  const achievementsList = [
+    {
+      metric: "Projects",
+      value: "7",
+      postfix: "+",
+    },
+    {
+      metric: "Awards",
+      value: "2",
+    },
+    {
+      metric: "Years",
+      value: "2",
+    },
+  ];
+
   return (
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
       <div className="sm:border-blue-100 sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between ">
@@ -36,17 +31,20 @@ export default function AchievementsSession() {
             className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
           >
             <h2 className="text-4xl font-bold flex flex-row">
-              {achievement.prefix}
               <AnimatedNumbers
                 includeComma
                 transitions={(index) => ({
                   type: "spring",
                   duration: index + 0.3,
+                  mass: 1,
+                  friction: 100,
+                  tensions: 140 * (index + 1),
                 })}
                 animateToNumber={parseInt(achievement.value)}
                 fontStyle={{
                   fontSize: 40,
                 }}
+                locale="en-US"
               />
               {achievement.postfix}
             </h2>
